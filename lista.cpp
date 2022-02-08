25e9a8fc9630f1315cf75abb3f421279464f3fa2
Ana Paula <54125389+anahoog@users.noreply.github.com>
2022-02-08 15:06:33 -0300

    Create lista.cpp

---
+++ lista.cpp
@@ -0,0 +1,14 @@
+#include "lista.h"
+
+Lista cria_lista(){
+    Lista nova;
+
+    nova.len = 0;
+    nova.primeiro = nullptr;
+
+    return nova;
+}
+
+void lista_anexa(Lista & l, const string & dado){
+
+}
